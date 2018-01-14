f0af338945ecaf7e71fa7ca885e73473979b803f
Malcolm Jones <malcolm@adobe.com>
2018-01-13 19:35:50 -0500

    Fix: tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_environment_file(host):
+    f = host.file('/etc/environment')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.contains('PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"')
